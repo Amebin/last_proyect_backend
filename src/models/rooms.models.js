99889f59369e093996505810cefa9554906c76be
Main <gimenezfrancotomas96@gmail.com>
2023-09-15 21:55:25 -0300

    modificacion de las rooms models

--- src/models/rooms.models.js
+++ src/models/rooms.models.js
@@ -7,7 +7,9 @@ const collection = 'rooms'
 const schema = new mongoose.Schema({
     title: { type: String, required: true },
     price: { type: String, required: true },
-    image: { type: String }
+    images: [{ type: Array }],
+    description: { type: String, required: true },
+    avaliableDates: { type: Date, required: true },
 });
 
 const roomModel = mongoose.model(collection, schema);
