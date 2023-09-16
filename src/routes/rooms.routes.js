d59acae645d6248175b5c3020c74da27824432de
Main <gimenezfrancotomas96@gmail.com>
2023-09-15 21:56:00 -0300

    creacion de todos los endpoints de rooms routes

--- src/routes/rooms.routes.js
+++ src/routes/rooms.routes.js
@@ -18,10 +18,10 @@ export const roomsRoutes = ()  => {
         res.status(200).send({ status: 'OK', data: rooms })
     })
 
-    router.get('/one/:cid', async (req, res) => {
+    router.get('/one/:rid', async (req, res) => {
         try {
-            if (mongoose.Types.ObjectId.isValid(req.params.cid)) {
-                const room = await roomModel.findById(req.params.cid)
+            if (mongoose.Types.ObjectId.isValid(req.params.rid)) {
+                const room = await roomModel.findById(req.params.rid)
 
                 if (room === null) {
                     res.status(404).send({ status: 'ERR', data: 'No existe una habitacion con ese ID' })
@@ -36,7 +36,61 @@ export const roomsRoutes = ()  => {
         }
     })
 
-    
+    router.post('/', validateCreateFields, async (req, res) => {
+        if (validationResult(req).isEmpty()) {
+            try {
+                const { title, price, images, description, avaliableDates } = req.body
+                const avaliableDate = new Date(req.body.avaliableDates)
+                const newRoom = { title, price, images, description, avaliableDates: avaliableDate }
+
+                const process = await roomModel.create(newRoom)
+                
+                res.status(201).send({ status: 'Created', data: process })
+            } catch (err) {
+                res.status(500).send({ status: 'ERR', data: err.message })
+            }
+        } else {
+            res.status(400).send({ status: 'ERR', data: validationResult(req).array() })
+        }
+    })
+
+    router.put('/:rid', async (req, res) => {
+        try {
+            const id = req.params.rid
+            if (mongoose.Types.ObjectId.isValid(id)) {
+                const roomToModify = await roomModel.findOneAndUpdate({ _id: id }, { $set: req.body }, { new: true })
+
+                if (!roomToModify) {
+                    res.status(404).send({ status: 'ERR', data: 'No existe tarjeta con ese ID' })
+                } else {
+                    res.status(200).send({ status: 'OK', data: roomToModify })
+                }
+            } else {
+                res.status(400).send({ status: 'ERR', data: 'Formato de ID no válido' })
+            }
+        } catch (err) {
+            res.status(500).send({ status: 'ERR', data: err.message })
+        }
+    })
+
+    router.delete('/:rid', async (req, res) => {
+        try {
+            const id = req.params.rid
+            if (mongoose.Types.ObjectId.isValid(id)) {
+                const roomToDelete = await roomModel.findOneAndDelete({ _id: id });
+
+                if (!roomToDelete) {
+                    res.status(404).send({ status: 'ERR', data: 'No existe tarjeta con ese ID' })
+                } else {
+                    res.status(200).send({ status: 'OK', data: roomToDelete })
+                }
+            } else {
+                res.status(400).send({ status: 'ERR', data: 'Formato de ID no válido' })
+            }
+        } catch (err) {
+            res.status(500).send({ status: 'ERR', data: err.message })
+        }
+    })
 
     return router
 }
